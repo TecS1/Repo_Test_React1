d138996e7de36fbd3be5ffc5b8c42d2b26101da0
TecS1 <tec1sierra@gmail.com>
2023-11-07 17:22:13 -0500

    base de targetas finitas
    jsjsjs

--- src/PdfTest1.jsx
+++ src/PdfTest1.jsx
@@ -6,13 +6,21 @@ export default function PdfTest() {
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry =>{
-            var the_element = entry.target.children[0]
+            //var the_element = entry.target.children[0]
+            if (entry.isIntersecting) {
+                console.log("Se ve o intersecta xD: " + entry.target.children[0]);
+                entry.target.children[0].classList.add("show")
+                observer.unobserve(entry.target)
+            }else{
+                entry.target.children[0].classList.remove("show")                
+            }
             //var the_element = entry.target
-            the_element.classList.toggle("show", entry.isIntersecting)
+            //the_element.classList.toggle("show", entry.isIntersecting)
+            //entry.target.children[0].classList.toggle("show", entry.isIntersecting)
         })
     },{
-        root: document.querySelector(".card-container"),
-        threshold: 1,
+        root: document.querySelector(".card-container"),        
+        threshold: 0.5,
     })
 
     useEffect(() => {
@@ -22,6 +30,7 @@ export default function PdfTest() {
         cards.forEach(card =>{            
             observer.observe(card)
         })
+        //observer.root.style.border = "2px solid #44aa44";
     }, []);
 
     return(
